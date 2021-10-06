d710a00a72785a275a0c5fa2653a228c494039b3
Akulav <43496076+Akulav@users.noreply.github.com>
2021-10-07 01:34:29 +0300

    Added SignUp and Login Logic

--- LoginService/LoginService/Database.cs
+++ LoginService/LoginService/Database.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
@@ -7,11 +8,13 @@ namespace LoginService
 {
     class Database
     {
-        public static void login(string username, string password)
+        public static void signup(string username, string password)
         {
             Random rnd = new Random();
-            var id = rnd.Next(20, 200);
-            string query = $"INSERT INTO [dbo].[Data] (id, username, password) VALUES ('{id}','{username}','{password}')";
+            var id = rnd.Next(20, 2000);
+            //string query = $"INSERT INTO [dbo].[Data] (id, username, password) VALUES ('{id}','{username}','{password}')";
+
+            string query = $"IF NOT EXISTS (SELECT * FROM [dbo].[Data] WHERE username = '{username}') BEGIN INSERT INTO [dbo].[Data] (id, username, password) VALUES ('{id}','{username}','{password}') END";
 
             string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\akula\Documents\users.mdf; Integrated Security = True; Connect Timeout = 30";
             SqlConnection connection = new SqlConnection(connectionString);
@@ -20,9 +23,17 @@ namespace LoginService
             cmd.ExecuteNonQuery();
         }
 
-        public static void signup(string username, string password)
+        public static void login(string username, string password)
         {
+            string query = $"SELECT COUNT(id)FROM [dbo].[Data] WHERE username = '{username}' AND password = '{password}'";
 
+            string connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\akula\Documents\users.mdf; Integrated Security = True; Connect Timeout = 30";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            
+            string result = cmd.ExecuteScalar().ToString();
+            Console.WriteLine(result);
         }
     }
 }
